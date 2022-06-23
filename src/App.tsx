35eb865fdc63688c284e3544670f12540c78d724
Santos Pierre <santospierre1305@gmail.com>
2022-06-23 21:08:47 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+		<div className="">
+			<h1 className='text-6xl font-black md:bg-red-600 bg-cyan-500'>
+				Hello world! 2
+			</h1>
+		</div>
+  )
+}
+
+export default App
